b39b68bf291180112c3cbd1c26c74d4d52568a31
Matvey0808 <avto.mat.2000g@gmail.com>
2026-04-16 12:49:27 +0700

    fix: JourneyDialog
    - carried out style button and OutlineInputBorder from TextField
    - add dialog in new file dialog_widget.dart
    - add alertDialog

--- lib/view/journeys_view.dart
+++ lib/view/journeys_view.dart
@@ -1,3 +1,4 @@
+import 'package:bonbagage/widget/dialog_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:bonbagage/widget/journeys_card_widget.dart';
 
@@ -6,92 +7,6 @@ class JourneysView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    void JourneyAddDialog() {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) => Dialog(
-          child: Container(
-            height: width * 0.6,
-            child: Column(
-              children: [
-                SizedBox(height: 10),
-                SizedBox(
-                  width: width * 0.6,
-                  child: TextField(
-                    cursorColor: Colors.black45,
-                    decoration: InputDecoration(
-                      hintText: "start date",
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(width: 2, color: Colors.black45),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(width: 2, color: Colors.black45),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                SizedBox(
-                  width: width * 0.6,
-                  child: TextField(
-                    cursorColor: Colors.black45,
-                    decoration: InputDecoration(
-                      hintText: "end date",
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(width: 2, color: Colors.black45),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(width: 2, color: Colors.black45),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white70,
-                        elevation: 0,
-                        shadowColor: Colors.transparent,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        "Отмена",
-                        style: TextStyle(color: Colors.black45),
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white70,
-                        elevation: 0,
-                        shadowColor: Colors.transparent,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        "Добавить",
-                        style: TextStyle(color: Colors.black45),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
-    }
-
     return Scaffold(
       body: ListView(
         children: [
@@ -117,7 +32,7 @@ class JourneysView extends StatelessWidget {
         elevation: 0,
         highlightElevation: 0,
         onPressed: () {
-          JourneyAddDialog();
+          journeyDialog(context);
         },
         child: const Icon(Icons.add, color: Colors.black54, size: 25),
       ),
